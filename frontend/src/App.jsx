06690d3ed1d695f3f51622e07567d48e2b0cf247
EdwardVuong-75 <Vuonghoanganhtuan01@gmail.com>
2025-11-19 01:04:16 +1300

    Building Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,31 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  const [message, setMessage] = useState('');
+// we’re installing Bootstrap for styling,
+//  Axios for making HTTP requests,
+//  and react-router-dom for handling client-side routing.
 
-  useEffect(() => {
-    fetch('/api/')
-      .then(res => res.json())
-      .then(data => setMessage(data.message));
-  }, []);
+function Login() {
 
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const navigate = useNavigate()
+  
   return (
     <div className="App">
-      <h1>MERN Stack App</h1>
-      <p>{message}</p>
+      <h1>Welcome</h1>
+      <div>
+        <input type='text' id = 'myinput' placeholder = "Enter your Email "></input>
+        <br/>
+        <input type='text' id = 'myinput2' placeholder = "Enter your password "></input>
+        <br/>
+        <button type = 'submit' className = 'sbmt' onClick={() => console.log('ff')}>Sign in</button>
+        <br/>or<br/>
+        <button>Create Account</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Login;
---
+++ frontend/src/MainPage.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function MainPage() {
+  
+  return (
+    <div>
+     Ok
+    </div>
+  );
+}
+
+export default MainPage;
