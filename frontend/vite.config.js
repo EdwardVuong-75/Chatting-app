ba49e0293dbf2cc458cefa4a778cadb91cede426
EdwardVuong-75 <Vuonghoanganhtuan01@gmail.com>
2026-09-25 01:25:09 +1200

    adding docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const inDocker = process.env.DOCKER === '1'
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true
+    host: true,
+    port: 5173,
+    open: !inDocker,
+    watch: inDocker ? { usePolling: true } : undefined,
   }
 })
